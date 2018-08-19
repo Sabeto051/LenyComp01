9ed91f5fdda6bef71905e733d71d7e9c95d2c17e
Sabeto051 <bedoya.santi@gmail.com>
2018-08-18 23:51:52 -0500

    Cambio a separador ;

--- Random/arreglarArchivo.py
+++ Random/arreglarArchivo.py
@@ -1,7 +1,7 @@
 with open('Random/Tabla_de_simbolos.txt') as file:
     tabla = [[str(element) for element in line.split(',')]for line in file]
 
-tabla2 = ''.join([ '-'.join(line) for line in tabla]   )
+tabla2 = ''.join([ ';'.join(line) for line in tabla]   )
 
 
 with open ('Random/Tabla_de_simbolos2.txt', 'w') as file:
